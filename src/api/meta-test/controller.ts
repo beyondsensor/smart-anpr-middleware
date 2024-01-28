3f50c7321b26aafde02cf94036baac1695907291
Foo Yong Yan <fooyongyan@gmail.com>
2024-01-28 17:47:49 +0800

    Implemented API for testing

--- src/api/meta-test/controller.ts
+++ src/api/meta-test/controller.ts
@@ -2,29 +2,35 @@ import { Request, Response } from "express"
 import service from "./service"
 import appLogger from "../../lib/logger";
 
-async function GetAll ( request : Request, response : Response  ) { 
-    const res = await service.GetAll(); 
-    response.send ( res)
+async function GetAll(request: Request, response: Response) {
+    const res = await service.GetAll();
+    response.send(res)
 }
 
-async function Update ( request : Request, response : Response  ) { 
-    const res = await service.GetAll(); 
-    response.send ( res)
+async function Update(request: Request, response: Response) {
+    console.log("Update ")
+    const res = await service.GetAll();
+    response.send(res)
 }
 
-async function Create ( request : Request, response : Response  ) { 
-    const res = await service.GetAll(); 
-    response.send ( res)
+async function Create(request: Request, response: Response) {
+    const { name } = request.body;
+    console.log(request.file)
+    console.log(name)
+    if (request.file)
+        service.Create(name, request.file);
+
+    response.send({})
 }
-async function Delete ( request : Request, response : Response  ) { 
-    const res = await service.GetAll(); 
-    response.send ( res)
+async function Delete(request: Request, response: Response) {
+    const res = await service.GetAll();
+    response.send(res)
 }
 
 
-export default { 
-    GetAll, 
-    Update, 
-    Create, 
+export default {
+    GetAll,
+    Update,
+    Create,
     Delete
 }
--- src/api/meta-test/router.ts
+++ src/api/meta-test/router.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import controller from "./controller";
+import multer from 'multer';
+
 const metaTestRouter = express.Router();
 
+// Create an instance of multer middleware
+const upload = multer();
 metaTestRouter.get("/api/meta-tests", controller.GetAll);
 metaTestRouter.post("/api/meta-tests", controller.Create);
-metaTestRouter.put("/api/meta-tests", controller.Create);
+metaTestRouter.put("/api/meta-tests", upload.single('snapshot'), controller.Create);
 metaTestRouter.delete("/api/meta-tests", controller.Delete);
 metaTestRouter.patch("/api/meta-tests", controller.Update);
 
